21c8b09297f8cdeb77c7f18ca59b445abfe14954
Raninder Dhillon <37101702+raninder@users.noreply.github.com>
2022-04-12 21:08:22 -0400

    Delete nextjs-test/hendrixer-github-learn directory

--- nextjs-test/hendrixer-github-learn/pages/api/data.js
+++
@@ -1,8 +0,0 @@
-// pages/api/data.js
-// route => /api/data
-
-export default (req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify({ message: 'hello' }))
-}
